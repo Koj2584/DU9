2ae00129c8dd749b6e414e894e688701631a014c
Koj2584 <vomelaj.05@spst.eu>
2023-02-14 22:12:47 +0100

    Nahrání na github

--- File stream/File stream/Form2.Designer.cs
+++ File stream/File stream/Form2.Designer.cs
@@ -31,6 +31,8 @@
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -46,30 +48,51 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(13, 13);
+            this.listBox1.Location = new System.Drawing.Point(13, 26);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(375, 394);
+            this.listBox1.Size = new System.Drawing.Size(375, 381);
             this.listBox1.TabIndex = 1;
             // 
             // listBox2
             // 
             this.listBox2.FormattingEnabled = true;
-            this.listBox2.Location = new System.Drawing.Point(394, 13);
+            this.listBox2.Location = new System.Drawing.Point(394, 26);
             this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(394, 394);
+            this.listBox2.Size = new System.Drawing.Size(394, 381);
             this.listBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Původní";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(391, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Opravený";
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.listBox2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form2";
             this.Text = "Form2";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -78,5 +101,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- File stream/File stream/Form2.cs
+++ File stream/File stream/Form2.cs
@@ -20,16 +20,34 @@ namespace File_stream
 
         private void button1_Click(object sender, EventArgs e)
         {
-            OpenFileDialog file = new OpenFileDialog();
-
-            if (file.ShowDialog() == DialogResult.OK)
+            int soucet = 0, pocetLich = 0, pocetSud = 0;
+            StreamReader ctenar = new StreamReader("cislo.txt", Encoding.GetEncoding("windows-1250"));
+            listBox1.Items.Clear();
+            while (!ctenar.EndOfStream)
             {
-                StreamReader ctenar = new StreamReader(file.FileName, Encoding.GetEncoding("windows-1250"));
-                listBox1.Items.Clear();
-                while (!ctenar.EndOfStream)
-                    listBox1.Items.Add(ctenar.ReadLine());
-                ctenar.Close();
+                listBox1.Items.Add(ctenar.ReadLine());
+                int cislo = 0;
+                string vstup = listBox1.Items[listBox1.Items.Count - 1].ToString();
+                cislo = int.Parse(vstup);
+                if (cislo % 2 == 0 && cislo != 0)
+                    pocetSud++;
+                else
+                    pocetLich++;
+                soucet += cislo;
             }
+            ctenar.Close();
+
+            StreamWriter zapis = new StreamWriter("cislo.txt", true, Encoding.GetEncoding("windows-1250"));
+            zapis.WriteLine("\n"+soucet);
+            zapis.WriteLine(pocetLich);
+            zapis.WriteLine(pocetSud);
+            zapis.Close();
+
+            ctenar = new StreamReader("cislo.txt", Encoding.GetEncoding("windows-1250"));
+            listBox2.Items.Clear();
+            while (!ctenar.EndOfStream)
+                listBox2.Items.Add(ctenar.ReadLine());
+            ctenar.Close();
         }
     }
 }
